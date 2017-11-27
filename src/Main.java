a6610b889e5e9128d3a0b1e0261c3d12ec2f61ee
Hétfő <keszler.patricia@gmail.com>
2017-11-27 18:30:36 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by java1 on 2017.11.27..
+ */
+public class Main {
+    public static void main(String[] args) {
+
+
+        /**
+         *
+         */
+
+        System.out.print(c);
+    }
+}
